db068bbcff2e0f6a9df2fce147ef1ac36bfd1174
hejun <jun.he-01@legrand.com>
2024-10-31 17:25:04 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace SSHWinformApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtCMD = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(41, 27);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(117, 25);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.Text = "192.168.0.109";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "用户名";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(239, 27);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(117, 25);
+            this.txtUsername.TabIndex = 2;
+            this.txtUsername.Text = "root";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(439, 27);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(117, 25);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "123456";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(381, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "密码";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(588, 27);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(115, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 176);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1262, 468);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // txtCMD
+            // 
+            this.txtCMD.Location = new System.Drawing.Point(15, 120);
+            this.txtCMD.Name = "txtCMD";
+            this.txtCMD.Size = new System.Drawing.Size(515, 25);
+            this.txtCMD.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "COMMAND";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(588, 120);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 4;
+            this.btnExecute.Text = "执行";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 656);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCMD);
+            this.Controls.Add(this.txtServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtCMD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExecute;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSHWinformApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.SelectionFont = new Font("Arial", 12, FontStyle.Bold);
+            richTextBox1.SelectionColor = Color.Red;
+        }
+        SshClient sshClient = null;
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (sshClient != null)
+            {
+                sshClient.Disconnect();
+            }
+            sshClient = new SshClient(txtServer.Text, 22, txtUsername.Text, txtPassword.Text);
+            string s = sshClient.ConnectionInfo.Host; ;
+            //PrintLog(connMsg);
+
+            btnLogin.Text = "重新登录";
+        }
+
+        private void SshClient_ServerIdentificationReceived(object sender, Renci.SshNet.Common.SshIdentificationEventArgs e)
+        {
+          
+        }
+
+        public void Execute(string command)
+        {
+            try
+            {
+                if(!sshClient.IsConnected)
+                {
+                    sshClient.Connect();
+                }
+                // 执行命令
+                var result = sshClient.RunCommand(command);
+                PrintLog(result.Result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        public void PrintLog(string log)
+        {
+            // 将文本添加到RichTextBox并自动添加换行符
+            richTextBox1.AppendText(log + Environment.NewLine);
+
+            // 自动滚动到RichTextBox的底部
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            Execute(txtCMD.Text);
+        }
+    }
+}
